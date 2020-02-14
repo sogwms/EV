3478f5e72a3c70099bb611aa5c731838d67afece
sogw <sogwyms@gmail.com>
2020-02-14 10:56:41 +0800

    Add inside version info

--- core/pss.c
+++ core/pss.c
@@ -1,8 +1,10 @@
-/*
+/**
  * Copyright (c) 2019, sogwyms@gmail.com
  *
  * SPDX-License-Identifier: Apache-2.0
  *
+ * @version 0.1.0
+ * 
  * Change Logs:
  * Date           Author       Notes
  * 2019-12-01     sogwms       The first version
